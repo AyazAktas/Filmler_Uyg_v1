4e59462c22507211a2699c3b0885c5a1f7ed0f33
AyazAktaş <ayazaktastr@gmail.com>
2024-07-23 14:18:17 +0300

    filmler uygulaması son hal

--- app/src/main/java/com/example/filmlerapp/ui/adapter/FilmlerAdapter.kt
+++ app/src/main/java/com/example/filmlerapp/ui/adapter/FilmlerAdapter.kt
@@ -3,9 +3,12 @@ package com.example.filmlerapp.ui.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.filmlerapp.data.entity.Filmler
 import com.example.filmlerapp.databinding.CardTasarimBinding
+import com.example.filmlerapp.ui.fragment.AnasayfaFragmentDirections
+import com.google.android.material.snackbar.Snackbar
 
 
 class FilmlerAdapter(var mContext:Context,var filmlerListesi:List<Filmler>) :RecyclerView.Adapter<FilmlerAdapter.CardTasarimHolder>( ){
@@ -23,6 +26,16 @@ class FilmlerAdapter(var mContext:Context,var filmlerListesi:List<Filmler>) :Rec
         t.imageView.setImageResource(mContext.resources.getIdentifier(film.resim,"drawable",mContext.packageName))
         t.textViewFiyat.text="${film.fiyat} ₺"
         t.textViewFilmAdi.text="${film.ad}"
+        t.cardViewFilm.setOnClickListener{
+            val gecis=AnasayfaFragmentDirections.detayGecis(film=film)
+            Navigation.findNavController(it).navigate(gecis)
+
+        }
+
+        t.buttonSepet.setOnClickListener {
+            Snackbar.make(it,"${film.ad} sepete eklendi.",Snackbar.LENGTH_SHORT)
+                .show()
+        }
     }
 
     override fun getItemCount(): Int {
