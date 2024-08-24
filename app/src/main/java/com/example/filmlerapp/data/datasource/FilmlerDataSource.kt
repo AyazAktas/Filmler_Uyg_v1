f10bbadfc9aca1f23c8fd93b52183cd5bd4c4b17
AyazAktaş <ayazaktastr@gmail.com>
2024-08-24 12:31:57 +0300

    reading data and displaying it in the application

--- app/src/main/java/com/example/filmlerapp/data/datasource/FilmlerDataSource.kt
+++ app/src/main/java/com/example/filmlerapp/data/datasource/FilmlerDataSource.kt
@@ -1,26 +1,14 @@
 package com.example.filmlerapp.data.datasource
 
 import com.example.filmlerapp.data.entity.Filmler
+import com.example.filmlerapp.room.FilmlerDao
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class FilmlerDataSource {
+class FilmlerDataSource(var fdao:FilmlerDao) {
     suspend fun filmleriYukle():List<Filmler> =
         withContext(Dispatchers.IO)
         {
-            val filmlerListesi=ArrayList<Filmler>()
-            val f1=Filmler(1,"Pulp Fiction","pulp",24)
-            val f2=Filmler(2,"Sil Baştan","eternal",28)
-            val f3=Filmler(3,"LOTR","lotr",32)
-            val f4=Filmler(4,"Forrest Gump","forrest",36)
-            val f5=Filmler(5,"American Pyscho","american",20)
-            val f6=Filmler(6,"Saksı Olmanın","perks",16)
-            filmlerListesi.add(f1)
-            filmlerListesi.add(f2)
-            filmlerListesi.add(f3)
-            filmlerListesi.add(f4)
-            filmlerListesi.add(f5)
-            filmlerListesi.add(f6)
-            return@withContext filmlerListesi
+            return@withContext fdao.filmleriYukle()
         }
 }
--- app/src/main/java/com/example/filmlerapp/di/AppModule.kt
+++ app/src/main/java/com/example/filmlerapp/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.filmlerapp.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.filmlerapp.data.datasource.FilmlerDataSource
 import com.example.filmlerapp.data.repo.FilmlerRepository
+import com.example.filmlerapp.room.FilmlerDao
+import com.example.filmlerapp.room.Veritabani
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -19,8 +24,17 @@ class AppModule {
 
     @Provides
     @Singleton
-    fun provideFilmlerDataSource():FilmlerDataSource{
-        return FilmlerDataSource()
+    fun provideFilmlerDataSource(fdao:FilmlerDao):FilmlerDataSource{
+        return FilmlerDataSource(fdao)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideFilmlerDao(@ApplicationContext context:Context):FilmlerDao{
+        val vt= Room.databaseBuilder(context,Veritabani::class.java,"filmler_app.sqlite")
+            .createFromAsset("filmler_app.sqlite").build()
+        return vt.getFilmlerDao()
     }
 
 }
